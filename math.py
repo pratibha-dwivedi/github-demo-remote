cc242191fb5cefdd32946987767e343a103328b2
pratibha-dwivedi <99012985+pratibha-dwivedi@users.noreply.github.com>
2022-02-04 15:02:02 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+# Add implementation
+def add (x,y):
+    return x+y
+
+# Subtract implementation
+def subtract (x,y):
+    return x-y               # on master branch
+
+# Multiply implementation
+def multiply (x,y):
+    return x*y				#on bug456
+
+# Divide implementation
+def divide (x,y):
+    if y==0:				# master branch
+      return Divide_by_0_error
+    else:
+       return x/y
+#squareimplemented
+def square(x):
+     return x*x
